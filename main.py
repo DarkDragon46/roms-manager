c7ac053148b8d6ff83a14e4304fc29870b4535e7
unknown <billymastful@gmail.com>
2024-10-04 12:34:11 +0200

    testing gui

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox
+from login_form import Ui_login_form
+
+class Login(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_login_form()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.authenticate)
+
+        self.show()
+
+    def authenticate(self):
+        email = self.ui.lineEdit.text()
+        password = self.ui.lineEdit_2.text()
+
+        if email == 'john@test.com' and password == '123456':
+            QMessageBox.information(self, 'Success!', 'You Are logged in.')
+        else:
+            QMessageBox.critical(self, 'Error', "invalid email or password")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login_window = Login()
+    sys.exit(app.exec())
+
