2a5a5f8f49ef3c61afe487bca0aac4c355f1121c
unknown <billymastful@gmail.com>
2024-10-04 17:08:18 +0200

    added first ui and icons

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox
-from ui.login_form import Ui_login_form
+from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox, QMainWindow
+from ui.output import Ui_MainWindow
 
-class Login(QWidget):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-
-        self.ui = Ui_login_form()
-        self.ui.setupUi(self)
-
-        self.ui.pushButton.clicked.connect(self.authenticate)
-
+        self.setupUi(self)
         self.show()
 
-    def authenticate(self):
-        email = self.ui.lineEdit.text()
-        password = self.ui.lineEdit_2.text()
 
-        if email == 'john@test.com' and password == '123456':
-            QMessageBox.information(self, 'Success!', 'You Are logged in.')
-        else:
-            QMessageBox.critical(self, 'Error', "invalid email or password. Please change.")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    login_window = Login()
-    sys.exit(app.exec())
-
+    window = MainWindow()
+    app.exec()
